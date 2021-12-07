3152ab61662032af37981c4e8f9cb63e2225bb8c
jesusmongedev <jesus_jaml@outlook.com>
2021-12-07 15:33:17 -0500

    Rooms BEM naming convention and Styles

--- src/Rooms/index.js
+++ src/Rooms/index.js
@@ -1,4 +1,4 @@
-import "./style.css";
+import "./style.scss";
 import React, { useState } from 'react';
 
 const items = [
@@ -6,19 +6,19 @@ const items = [
       name: "Deluxe",
       description:
         "Lorem ipsum dolor sit amet consectetur adipiscing elit eget litora vehicula sociosqu, praesent in magnis eu pharetra mauris congue varius consequat nulla, arcu neque porttitor inceptos faucibus mollis pellentesque nec per interdum.",
-      image: "https://i.imgur.com/uz4qrhX.png?1",
+      image: "https://i.postimg.cc/Gt4CBcgg/nad-hemnani-LTwm-Gm1-WXa-M-unsplash.jpg",
     },
     {
       name: "Standard",
       description:
         "Lorem ipsum dolor sit amet consectetur adipiscing elit eget litora vehicula sociosqu, praesent in magnis eu pharetra mauris congue varius consequat nulla, arcu neque porttitor inceptos faucibus mollis pellentesque nec per interdum.",
-      image: "https://i.imgur.com/uz4qrhX.png?1",
+      image: "https://i.postimg.cc/7YT4wZ2f/sara-dubler-Koei-7y-Yt-Io-unsplash.jpg",
     },
     {
       name: "First Class",
       description:
         "Lorem ipsum dolor sit amet consectetur adipiscing elit eget litora vehicula sociosqu, praesent in magnis eu pharetra mauris congue varius consequat nulla, arcu neque porttitor inceptos faucibus mollis pellentesque nec per interdum.",
-      image: "https://i.imgur.com/uz4qrhX.png?1",
+      image: "https://i.postimg.cc/2SRD6NQT/patrick-robert-doyle-AH8z-KXq-FITA-unsplash.jpg",
     },
   ];
 
@@ -28,17 +28,17 @@ var count=0;
 const Rooms = () => {
     const [count, setCount] = useState(0);
     return(
-        <section>
+        <section className="rooms">
             <h2>Habitaciones</h2>
             <div className="rooms__container">
                 <img src={items[count].image} alt={items[count].name} />
-                <h3>{items[count].name}</h3>
-                <p>{items[count].description}</p>
-                <div className="rooms__container__button">
+                <div className="rooms__container-button">
                     <button onClick={() => setCount(0)}></button>
                     <button onClick={() => setCount(1)}></button>
                     <button onClick={() => setCount(2)}></button>
                 </div>
+                <h3>{items[count].name}</h3>
+                <p>{items[count].description}</p>
                 <button className="vermas">Ver más</button>
             </div>
         </section>
