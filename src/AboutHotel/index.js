d2adf6e29c2f542b3ae64299ee0211bc09b19275
jesusmongedev <jesus_jaml@outlook.com>
2021-12-08 12:04:45 -0500

    Cree el componente AboutHotel. Selectores con Metodología BEM y estilos para Diseño Mobile

---
+++ src/AboutHotel/index.js
@@ -0,0 +1,15 @@
+ import './AboutHotel.scss'
+
+ const AboutHotel = () => {
+    return (
+        <div className="about-hotel">
+            <h1 className="about-hotel__title">SOBRE EL HOTEL</h1>
+            <p className="about-hotel__description"> 
+                Lorem ipsum dolor sit amet consectetur adipiscing elit eget litora vehicula sociosqu, praesent in magnis eu pharetra mauris congue varius consequat nulla, arcu neque porttitor inceptos faucibus mollis pellentesque nec per interdum. Primis neque ante lobortis semper blandit dictum, volutpat proin augue vulputate varius pretium, arcu mollis facilisis aptent magna. Convallis tempus posuere urna fames hac taciti, molestie ...
+            </p>
+            <button className="button">Ver más</button>
+        </div>
+    )
+}
+
+export {AboutHotel}
