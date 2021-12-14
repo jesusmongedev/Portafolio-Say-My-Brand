ef3e39e4e23dc72791302fd3d5b3f706eb5e84c7
jesusmongedev <jesus_jaml@outlook.com>
2021-12-13 19:09:18 -0500

    Para conectarnos a la base de datos en contentful usamos los siguientes paquetes npm i contentful & npm i contentful-management

--- src/App.js
+++ src/App.js
@@ -6,14 +6,24 @@ import { Opinions } from "./Opinions";
 import { Footer } from "./Footer";
 import { ImageSlider } from "./ImageSlider";
 import { NewOpinion } from "./NewOpinion";
-import { useState } from "react";
+
+import { useEffect, useState } from "react";
+
 import { AboutHotel } from "./AboutHotel";
 import { RegisterModal } from "./RegisterModal";
 import { RegisterModalForm } from "./RegisterModalForm";
+import { useContentful } from "./ContentfulApi/main";
 
 function App() {
   const [openOpinions, setOpenOpinions] = useState(false);
   const [openRegister, setOpenRegister] = useState(false);
+
+  const[authors, setAuthors] = useState([]);
+  const { getAuthors } = useContentful();
+
+  useEffect(() => {
+    getAuthors().then((response) => console.log(response));
+  });
 
   openRegister? document.body.classList.add('active-modal') : document.body.classList.remove('active-modal')
 
