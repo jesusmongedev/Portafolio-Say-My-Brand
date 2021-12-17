10393a387e243dae06c483703132a66c6c2edab5
Jesús Monge <jesus_jaml@outlook.com>
2021-12-16 19:59:46 -0500

    Avance para completar el user's Journey, igual al prototipo en Figma

--- src/Booking/index.js
+++ src/Booking/index.js
@@ -1,14 +1,23 @@
-import './Booking.scss';
+import "./Booking.scss";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 
 const Booking = () => {
   return (
     <form className="booking">
       <h2 className="booking__title">HAGA SU RESERVACIÓN</h2>
-      <div className="booking__inputs"> 
-        <input type="date" id="check-in"  className="button" />
-        <input type="date" id="check-out" className="button" />
+      <div className="booking__inputs">
+        <label htmlFor="date" className="button">
+          
+          Check in <MdOutlineKeyboardArrowDown />
+        </label>
+        <label className="button">
+          
+          Check out <MdOutlineKeyboardArrowDown />
+        </label>
+        <input type="date" id="date" className="button button--dates" />
+        {/* <input type="date" id="check-in" className="button button--dates" /> */}
         <button type="submit" className="button button--active">
-            <a href="#" >RESERVAR</a>
+          <a href="#">RESERVAR</a>
         </button>
       </div>
     </form>
